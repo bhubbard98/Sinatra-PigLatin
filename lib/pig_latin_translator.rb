02f6a9b5a7cfef9a73ea7110ba4a984804f90d91
Brad Hubbard <brad.hubbard77@gmail.com>
2014-02-19 15:42:37 -0600

    all but 1st test

--- lib/pig_latin_translator.rb
+++ lib/pig_latin_translator.rb
@@ -1,8 +1,16 @@
 class PigLatinTranslator
 
   def translate(word)
-    if/y/.match(word[0])
-      word.push(word.shift(word[0]))
+    if /y/.match(word[0])
+      return word[1,word.length] + "yay"
+    elsif word.include?("y")
+      return word[word.index("y"),word.length] + word[0,word.index("y")] + "ay"
+    elsif /[aeiou]/.match(word[0])
+      return word + "way"
+    elsif
+      return + "ay"
+
+
     end
   end
 end
--- spec/pig_latin_translator_spec.rb
+++ spec/pig_latin_translator_spec.rb
@@ -10,25 +10,25 @@ describe PigLatinTranslator do
 
   describe '#translate' do
     context "the word begins with consonants" do
-      xit "removes leading consonants before first vowel, appending consonants + 'ay' to the end of the word" do
+      it "removes leading consonants before first vowel, appending consonants + 'ay' to the end of the word" do
       expect(translator.translate("glove")).to eq("oveglay")
     end
     end
 
     context "the word begins with vowels" do
-      xit "adds 'way' to the end of the word" do
+      it "adds 'way' to the end of the word" do
       expect(translator.translate("egg")).to eq("eggway")
     end
     end
 
     context "'y' is encountered before a vowel" do
       context "'y' is the first letter in the word" do
-        xit "removes 'y' and appends it to the end of the string followed by 'ay'"do
+        it "removes 'y' and appends it to the end of the string followed by 'ay'"do
         expect(translator.translate("yellow")).to eq("ellowyay")
       end
       end
       context "'y' is not the first letter in the word" do
-        xit "removes the preceding consonants and appends them to the end of the word followed by 'ay'" do
+        it "removes the preceding consonants and appends them to the end of the word followed by 'ay'" do
         expect(translator.translate("rhythm")).to eq("ythmrhay")
       end
      end
